f8be47a90f8afe6c6572f433b4f045902b741bb2
Will Eaton <me@wseaton.com>
2023-01-09 17:41:51 -0500

    swap to blocking api

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 pub mod response;
 
-use reqwest::{Client as ReqwestClient, Response};
+use reqwest::blocking::{Client as ReqwestClient, Response};
+
 use response::*;
 use serde::de::DeserializeOwned;
 
@@ -26,17 +27,17 @@ impl Client {
     //  - Implement own errors
     //  - Implement paging
     //  - Add client builder
-    pub async fn query<T>(&self, query_str: &str) -> Result<Vec<T>, reqwest::Error>
+    pub fn query<T>(&self, query_str: &str) -> Result<Vec<T>, reqwest::Error>
     where
         T: DeserializeOwned,
     {
-        let mut response = self.initial_request(query_str).await?;
-        let mut response_body: QueryResults = response.json().await?;
+        let mut response = self.initial_request(query_str)?;
+        let mut response_body: QueryResults = response.json()?;
 
         let mut data = Vec::new();
         while let Some(next_uri) = response_body.next_uri {
-            response = self.next_request(&next_uri).await?;
-            response_body = response.json().await?;
+            response = self.next_request(&next_uri)?;
+            response_body = response.json()?;
             if let Some(rows) = response_body.data {
                 data.extend(rows.into_iter().map(|x| serde_json::from_value(x).unwrap()));
             }
@@ -44,7 +45,7 @@ impl Client {
         Ok(data)
     }
 
-    async fn initial_request(&self, query_str: &str) -> Result<Response, reqwest::Error> {
+    fn initial_request(&self, query_str: &str) -> Result<Response, reqwest::Error> {
         let conn_str = format!("{}:{}/v1/statement", &self.base_url, &self.port);
         let mut rb = self.http_client.post(conn_str).body(query_str.to_string());
 
@@ -52,10 +53,10 @@ impl Client {
             rb = rb.header("X-Trino-User", user);
         }
 
-        rb.send().await
+        rb.send()
     }
 
-    async fn next_request(&self, next_uri: &str) -> Result<Response, reqwest::Error> {
-        self.http_client.get(next_uri).send().await
+    fn next_request(&self, next_uri: &str) -> Result<Response, reqwest::Error> {
+        self.http_client.get(next_uri).send()
     }
 }
