1643139b0fe3ba6998d148d70dbfc283b4d588f3
Will Eaton <me@wseaton.com>
2023-01-18 15:28:55 -0500

    revert sync

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,6 @@
 pub mod response;
 
-use std::io::Read;
-
-use flate2::read::GzDecoder;
-use reqwest::blocking::{Client as ReqwestClient, Response};
+use reqwest::{Client as ReqwestClient, Response};
 
 use response::*;
 use serde::de::DeserializeOwned;
@@ -33,14 +30,14 @@ impl Client {
     //  - Implement paging
     //  - Add client builder
     #[tracing::instrument(skip(self))]
-    pub fn query<T>(&self, query_str: &str) -> Result<Vec<T>, reqwest::Error>
+    pub async fn query<T>(&self, query_str: &str) -> Result<Vec<T>, reqwest::Error>
     where
         T: DeserializeOwned,
     {
-        let mut response = self.initial_request(query_str)?;
+        let mut response = self.initial_request(query_str).await?;
         let headers = response.headers().clone();
         debug!("trino response headers: {:#?}", headers);
-        let raw_text = response.text()?;
+        let raw_text = response.text().await?;
         debug!("raw_text: {}", raw_text);
 
         let mut response_body: QueryResults = serde_json::from_str(&raw_text).unwrap();
@@ -48,8 +45,8 @@ impl Client {
 
         let mut data = Vec::new();
         while let Some(next_uri) = response_body.next_uri {
-            response = self.next_request(&next_uri)?;
-            response_body = response.json()?;
+            response = self.next_request(&next_uri).await?;
+            response_body = response.json().await?;
 
             if let Some(rows) = response_body.data {
                 debug!("rows: {:?}", rows);
@@ -60,7 +57,7 @@ impl Client {
     }
 
     #[tracing::instrument(skip(self, query_str))]
-    fn initial_request(&self, query_str: &str) -> Result<Response, reqwest::Error> {
+    async fn initial_request(&self, query_str: &str) -> Result<Response, reqwest::Error> {
         let conn_str = format!("{}:{}/v1/statement", &self.base_url, &self.port);
         let mut rb = self.http_client.post(conn_str).body(query_str.to_string());
 
@@ -68,12 +65,12 @@ impl Client {
             rb = rb.header("X-Trino-User", user);
         }
 
-        rb.send()
+        rb.send().await
     }
 
     #[tracing::instrument(skip(self))]
-    fn next_request(&self, next_uri: &str) -> Result<Response, reqwest::Error> {
+    async fn next_request(&self, next_uri: &str) -> Result<Response, reqwest::Error> {
         debug!("navigating to next_uri: {}", next_uri);
-        self.http_client.get(next_uri).send()
+        self.http_client.get(next_uri).send().await
     }
 }
